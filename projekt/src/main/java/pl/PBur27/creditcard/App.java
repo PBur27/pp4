7174b19ebc7cd06583ef7cb26a09030cd07f9070
Paweł Burda <114923840+PBur27@users.noreply.github.com>
2024-03-18 14:25:02 +0100

    Add files via upload

---
+++ projekt/src/main/java/pl/PBur27/creditcard/App.java
@@ -0,0 +1,11 @@
+package pl.PBur27.creditcard;
+
+public class App {
+    public static void main(String[] args) {
+        String name = "Paweł";
+
+        System.out.println(String.format("Hello %s", name));
+
+
+    }
+}
